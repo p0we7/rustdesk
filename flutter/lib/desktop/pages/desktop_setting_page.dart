d9e0eff24d12089790c32d9e17fdf375a41115f0
Peter <i@iu.vc>
2026-01-10 14:50:00 +0800

    change all app name to haigang

--- flutter/lib/desktop/pages/desktop_setting_page.dart
+++ flutter/lib/desktop/pages/desktop_setting_page.dart
@@ -2321,7 +2321,7 @@ class _AboutState extends State<_About> {
                         .marginSymmetric(vertical: 4.0)),
               InkWell(
                   onTap: () {
-                    launchUrlString('https://rustdesk.com/privacy.html');
+                    launchUrlString('https://to.hnhggp.com/privacy.html');
                   },
                   child: Text(
                     translate('Privacy Statement'),
@@ -2329,7 +2329,7 @@ class _AboutState extends State<_About> {
                   ).marginSymmetric(vertical: 4.0)),
               InkWell(
                   onTap: () {
-                    launchUrlString('https://rustdesk.com');
+                    launchUrlString('https://to.hnhggp.com');
                   },
                   child: Text(
                     translate('Website'),
@@ -2347,7 +2347,7 @@ class _AboutState extends State<_About> {
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Text(
-                            'Copyright © ${DateTime.now().toString().substring(0, 4)} Purslane Ltd.\n$license',
+                            'Copyright © ${DateTime.now().toString().substring(0, 4)} HaiGang.\n$license',
                             style: const TextStyle(color: Colors.white),
                           ),
                           Text(
